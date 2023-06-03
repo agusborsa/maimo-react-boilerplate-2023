393858398de25912dddbee209edb8388166f6bac
Agustin <agustinborsaumai@gmail.com>
2023-06-03 03:28:02 -0300

    first steps into footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <h2 className={Styles.footer_title}>
+        Para mas información
+      </h2>
+    </footer>
   )
 }
 
